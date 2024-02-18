e9741ec4930465d88ade138a1637662cf34aca52
生唐 <1814379725@qq.com>
2024-02-18 15:31:47 +0800

    pdf图片提取

--- src/views/components/views/PdfPreview/Canvas/index.vue
+++ src/views/components/views/PdfPreview/Canvas/index.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
+import versionsImg from '../lib/2to4.png'
 import PdfPreviewVue2 from './vue2.vue'
 import PdfPreviewVue3 from './vue3.vue'
 const activeName = ref('vue2')
@@ -8,6 +10,7 @@ const scale = ref(1)
 const total = ref(0)
 const pdfPreviewVue3Ref = ref(null)
 const pdfText = ref('')
+const pageImgs = ref([])
 const handleClick = (tab, event) => {
   console.log(tab, event)
 }
@@ -31,8 +34,12 @@ const pdfPrev = () => {
   }
   pageNum.value--
 }
+const pageNumInputChange = (val)=>{
+  pageNum.value = val
+}
 const getText = async () => {
   const res = await pdfPreviewVue3Ref.value.getPageText()
+  console.log(res);
   pdfText.value = res.finalString
 }
 const pdfZoomIn = () => {
@@ -41,24 +48,34 @@ const pdfZoomIn = () => {
 const pdfZoomOut = () => {
   scale.value -= 0.1
 }
+const getPageImg = async () => {
+  try {
+    const res = await pdfPreviewVue3Ref.value.getPageImg()
+    pageImgs.value = res
+  } catch (error) {
+    ElMessage.warning(error)
+  }
+}
 </script>
 
 <template>
-  <div style="display: flex">
-    <div style="flex: 1">
-      <el-button type="primary" @click="loadPdf">加载pdf</el-button>
+  <div style="display: flex;flex-wrap: wrap;">
+    <div style="width: 50%;">
+      <!-- <el-button type="primary" @click="loadPdf">加载pdf</el-button> -->
       <el-button type="success" @click="pdfNext">下一页</el-button>
       <el-button type="info" @click="pdfPrev">上一页</el-button>
+      <el-input-number class="el-button" style="padding: 0;" @change="pageNumInputChange" :min="1" :max="total || 1"  controls-position="right" placeholder="跳转到" />
       <el-button type="warning" @click="getText">获取文字</el-button>
       <el-button type="danger" @click="pdfZoomIn">放大</el-button>
-      <el-button type="danger" @click="pdfZoomOut">缩小</el-button>
+      <el-button type="danger" round @click="pdfZoomOut">缩小</el-button>
+      <el-button type="success" round @click="getPageImg">图片提取</el-button>
       <div>页码：{{ pageNum }} 总数：{{ total }} 比例： {{ scale }}</div>
     </div>
-    <div style="flex: 1.6">
+    <div style="width: 50%;">
       <h4>注意点:</h4>
       <p>
         1.项目中有两个版本的pdf依赖包 pdfjs-2.0.943-dist 与
-        pdfjs-4.0.379，当前按理使用的是pdfjs-2.0.943-dist
+        pdfjs-4.0.379，当前案例使用的是pdfjs-2.0.943-dist
       </p>
       <p>
         2.pdfjs 2-3版本
@@ -68,25 +85,36 @@ const pdfZoomOut = () => {
       <p>
         3.由于我用的依赖包是下载的，所以需要在public下引入包，html里引入/pdfjs-2.0.943-dist//build/pdf.js
       </p>
+
+    </div>
+    <div style="width: 50%;">
+      <h4>相关文章</h4>
+      <p>官网：<a href="https://mozilla.github.io/pdf.js/">https://mozilla.github.io/pdf.js/</a></p>
+      <p>
+        中文官网：<a
+          href="https://gitcode.gitcode.host/docs-cn/pdf.js-docs-cn/examples/index.html">https://gitcode.gitcode.host/docs-cn/pdf.js-docs-cn/examples/index.html</a>
+      </p>
+      <p>
+        CSDN文章<a
+          href="https://blog.csdn.net/IAIPython/article/details/134525898">https://blog.csdn.net/IAIPython/article/details/134525898</a>
+      </p>
+      <p>
+        文字图片提取<a
+          href="https://www.haorooms.com/post/js_pdfjs_imagetext">https://www.haorooms.com/post/js_pdfjs_imagetext</a>
+      </p>
+      <p>
+        JavaScript提取PDF图片<a href="https://zhuanlan.zhihu.com/p/680562472">https://zhuanlan.zhihu.com/p/680562472</a>
+      </p>
+    </div>
+    <div style="width: 50%;">
+      <h4>2与4版本切换：</h4>
+      <p>注释代码替换</p>
+      <el-image style="width: 70%; height: 100px" :src="versionsImg" :zoom-rate="1.2" :max-scale="7" :min-scale="0.2"
+        :preview-src-list="[versionsImg]" fit="cover" />
     </div>
   </div>
-  <div>
-    <h4>相关文章</h4>
-    <p>官网：<a href="https://mozilla.github.io/pdf.js/">https://mozilla.github.io/pdf.js/</a></p>
-    <p>
-      中文官网：<a
-        href="https://gitcode.gitcode.host/docs-cn/pdf.js-docs-cn/examples/index.html">https://gitcode.gitcode.host/docs-cn/pdf.js-docs-cn/examples/index.html</a>
-    </p>
-    <p>
-      CSDN文章<a
-        href="https://blog.csdn.net/IAIPython/article/details/134525898">https://blog.csdn.net/IAIPython/article/details/134525898</a>
-    </p>
-    <p>
-      文字图片提取<a
-        href="https://www.haorooms.com/post/js_pdfjs_imagetext">https://www.haorooms.com/post/js_pdfjs_imagetext</a>
-    </p>
-  </div>
-  <div><strong>*第五页才识别的出来!</strong></div>
+
+  <div><strong>*文字第五页才识别的出来!</strong></div>
   <div style="display: flex">
     <div style="flex: 1">
       <el-tabs v-model="activeName" @tab-click="handleClick">
@@ -107,7 +135,9 @@ const pdfZoomOut = () => {
       </div>
       <div class="img">
         <h4>图片识别结果</h4>
-        <div style="height: 200px; overflow-y: auto">{{ pdfText }}</div>
+        <div style="display: flex;">
+          <img style="width: 100px;margin: 2px;" :src="pageImg.src" alt="" v-for="pageImg in pageImgs" :key="pageImg.src">
+        </div>
       </div>
     </div>
   </div>
--- src/views/components/views/PdfPreview/Canvas/vue3.vue
+++ src/views/components/views/PdfPreview/Canvas/vue3.vue
@@ -10,6 +10,7 @@ defineOptions({ name: 'PagePdfPreviewCanvas' })
 import { nextTick, onMounted, ref, toRefs, watch, toRaw } from 'vue'
 const { pdfjsLib } = globalThis;
 pdfjsLib.GlobalWorkerOptions.workerSrc = '/lib/pdfjs-2.0.943-dist/build/pdf.worker.js';
+// pdfjsLib.GlobalWorkerOptions.workerSrc = '/lib/pdfjs-4.0.379/build/pdf.worker.mjs';
 
 const props = defineProps({
   url: {
@@ -61,6 +62,7 @@ const renderPage = (num) => {
   pageRendering = true;
   toRaw(pdfDoc.value).getPage(num).then(function (page) {
     const viewport = page.getViewport(scale.value);
+    // const viewport = page.getViewport({scale:scale.value});
     canvas.value.height = viewport.height
     canvas.value.width = viewport.width
 
@@ -84,7 +86,6 @@ const renderPage = (num) => {
 // 获取文字
 function getPageText () {
   return new Promise(function (resolve, reject) {
-    console.log(pageNum.value);
     toRaw(pdfDoc.value).getPage(pageNum.value).then(function (pdfPage) {
       pdfPage.getTextContent().then(function (textContent) {
         let textItems = textContent.items;
@@ -93,8 +94,85 @@ function getPageText () {
           let item = textItems[i];
           finalString += item.str + ' ';
         }
-        resolve({finalString,textContent});
+        resolve({ finalString, textContent });
       });
+    });
+  });
+}
+// 获取图片
+function getPageImg () {
+  return new Promise(function (resolve, reject) {
+    toRaw(pdfDoc.value).getPage(pageNum.value).then(async (pdfPage) => {
+      const operators = await pdfPage.getOperatorList();
+      const rawImgOperator = operators.fnArray.reduce((acc, curr, i) => {
+        if ([pdfjsLib.OPS.paintImageXObject, pdfjsLib.OPS.paintJpegXObject].includes(curr)) {
+          acc.push(operators.argsArray[i][0]);
+        }
+        return acc;
+      }, []);
+      console.log(rawImgOperator);
+      if (rawImgOperator.length === 0) {
+        reject('暂无图片')
+      }
+
+      const arrApi = rawImgOperator.map((imageName) => {
+        return new Promise((resolve2, reject2) => {
+          pdfPage.objs.get(imageName, async (image) => {
+            const resizeScale = 1 / 4;
+            const width = image.width * resizeScale;
+            const height = image.height * resizeScale;
+            resolve2({
+              src: image.src, width, height
+            })
+            // 2版本无下面的属性
+            // const bmp = image.bitmap;
+            // const resizeScale = 1 / 4;
+            // const width = bmp.width * resizeScale;
+            // const height = bmp.height * resizeScale;
+            // const canvas = new OffscreenCanvas(width, height);
+            // const ctx = canvas.getContext('bitmaprenderer');
+            // ctx.transferFromImageBitmap(bmp);
+            // const blob = await canvas.convertToBlob();
+            // const img = document.body.appendChild(new Image());
+            // img.width = width;
+            // img.height = height;
+            // img.src = URL.createObjectURL(blob);
+            // resolve2({
+            //   src: URL.createObjectURL(blob), width, height
+            // })
+          })
+        })
+      })
+      Promise.all(arrApi).then((res) => {
+        resolve(res)
+      })
+      // for (const imageName of rawImgOperator) {
+      //   pdfPage.objs.get(imageName, async (image) => {
+      //     const resizeScale = 1 / 4;
+      //     const width = image.width * resizeScale;
+      //     const height = image.height * resizeScale;
+      //     resolve({
+      //       src: image.src, width, height
+      //     })
+      //     // 2版本无下面的属性
+      //     // const bmp = image.bitmap;
+      //     // const resizeScale = 1/4;
+      //     // const width = bmp.width * resizeScale;
+      //     // const height = bmp.height * resizeScale;
+      //     // const canvas = new OffscreenCanvas(width, height);
+      //     // const ctx = canvas.getContext('bitmaprenderer');
+      //     // ctx.transferFromImageBitmap(bmp);
+      //     // const blob = await canvas.convertToBlob();
+      //     // const img = document.body.appendChild(new Image());
+      //     // img.width = width;
+      //     // img.height = height;
+      //     // img.src = URL.createObjectURL(blob);
+      //     // resolve({
+      //     //   src: URL.createObjectURL(blob), width, height
+      //     // })
+
+      //   });
+      // }
     });
   });
 }
@@ -115,7 +193,8 @@ watch(() => props,
   deep: true
 })
 defineExpose({
-  getPageText
+  getPageText,
+  getPageImg
 })
 </script>
 
